d06b8b1633481938d868f1292a0b525039ccf6c0
js <jagtarsinghwork@gmail.com>
2025-08-23 01:40:47 +0530

    changes in zod and ouput

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="TestFire: Closed testing for Play Store apps"
-        />
-      </Head>
-      <body className="bg-black">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         <meta
+//           name="description"
+//           content="TestFire: Closed testing for Play Store apps"
+//         />
+//       </Head>
+//       <body className="bg-black">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
