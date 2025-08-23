7c65d279d69387a538cae0cbd05f6621e567147f
js <jagtarsinghwork@gmail.com>
2025-08-24 01:05:13 +0530

    changes in lang and not found new items  changes in postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // ✅ yaha tailwind ke plugin ka naya package
-    autoprefixer: {},
+    tailwindcss: {},
   },
 };
 
